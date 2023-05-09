d003d373176a335fad4526bcb644961b10cb0e0a
Alan Estrada <alanestrada@Alans-MacBook-Air.local>
2023-05-09 15:10:54 -0600

    First commit

---
+++ src/api/article-listing/controllers/article-listing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-listing controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::article-listing.article-listing');
---
+++ src/api/article-listing/routes/article-listing.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * article-listing router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::article-listing.article-listing');
